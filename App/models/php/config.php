710d7f61b76a50d94e603efb5f6ea8a09d4b5756
Yon@s W <130277885+YonasWorkneh@users.noreply.github.com>
2024-07-01 06:35:18 -0700

    Add files via upload

---
+++ App/models/php/config.php
@@ -0,0 +1,7 @@
+<?php
+$HOST = "localhost";
+$USER = "root";
+$PASS = "Incorrect1995";
+$DATABASE = "hagurash";
+$hagurash = ["mail" => "yonasdegefu158@gmail.com", "admin" => "Yonas Workneh"];
+$COOKIE_EXPIRATION_DATE = time() + 3600 * 24 * 30;
