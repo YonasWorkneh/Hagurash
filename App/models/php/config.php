53eac24c5c99106385ec474dd6af18ab895e88ad
Yon@s W <130277885+YonasWorkneh@users.noreply.github.com>
2024-07-01 08:03:51 -0700

    Update config.php

--- App/models/php/config.php
+++ App/models/php/config.php
@@ -1,7 +1,7 @@
 <?php
-$HOST = "localhost";
+$HOST = "https://vneubnhfagxamcrrmrpc.supabase.co";
 $USER = "root";
-$PASS = "Incorrect1995";
+$PASS = "Inc@rrect_1995";
 $DATABASE = "hagurash";
 $hagurash = ["mail" => "yonasdegefu158@gmail.com", "admin" => "Yonas Workneh"];
 $COOKIE_EXPIRATION_DATE = time() + 3600 * 24 * 30;
